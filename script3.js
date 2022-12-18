fbcf4ac4f69d457574dcf4e9ddf2203774865bc4
Shivam Kumar <115492003+shivamkumar4344@users.noreply.github.com>
2022-12-18 11:20:48 +0530

    Add files via upload

---
+++ script3.js
@@ -0,0 +1,15 @@
+let inputName = prompt("Enter your name:-")
+console.log(inputName)
+const firstElement = document.getElementById("first")
+firstElement.innerHTML = "Hey my name is "+inputName+"."
+console.log(firstElement)
+let inputAge = prompt("Enter your Age:-")
+console.log(inputAge)
+const secondElement = document.getElementById("second")
+secondElement.innerHTML = "My age is "+inputAge+"."
+console.log(secondElement)
+const thirdElement = document.getElementById("third")
+let inputSub = prompt("Enter your favourite subject:-")
+console.log(inputSub)
+thirdElement.innerHTML = "My fav subject is "+inputSub+"."
+console.log(thirdElement)
